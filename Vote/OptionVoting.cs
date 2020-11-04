0d6e15d81de0e09187fd21f196672fc3dde74df8
Alim Nevilov <52937912+nevilov@users.noreply.github.com>
2020-11-04 21:12:24 +0300

    Added Validation

--- Vote/OptionVoting.cs
+++ Vote/OptionVoting.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +8,22 @@ namespace Vote
 {
     public class OptionVoting
     {
+        [Required]
+        public string titleVoting { get; set; }
+
+        [Required]
+        public string descriptionVoting { get; set; }
+
+        [Required]
+        public DateTime start_dateVoting { get; set; } = DateTime.Now;
+
+        [Required]
+        public DateTime end_dateVoting { get; set; } = DateTime.Now;
+
+        public bool haveOption = false;
+        public string additionalOption = "+ Add option";
+        public string propertyOptionButton = "btn btn-outline-secondary";
+
 
     }
 }
