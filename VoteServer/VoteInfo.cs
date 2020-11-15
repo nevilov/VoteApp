b75d91356d30d0df4ba38f9005b5e46bcb55abd7
Alim Nevilov <52937912+nevilov@users.noreply.github.com>
2020-11-15 18:58:57 +0300

    Added VoteInfo database

---
+++ VoteServer/VoteInfo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+using Vote.Models;
+
+namespace VoteServer
+{
+    public class VoteInfo
+    {
+       [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+       [Key]
+    
+        public string TitleVoting { get; set; }
+
+        public string DescriptionVoting { get; set; }
+
+        public DateTime Start_dateVoting { get; set; } = DateTime.Now;
+
+        public DateTime End_dateVoting { get; set; } = DateTime.Now;
+
+        public string[] listOptions = new string[5];
+
+
+    }
+}
