6e8b3fa0b20fe3b806d779bf417337694e8936a4
Алим Невилов <nevilplay@mail.ru>
2021-01-04 18:21:15 +0300

    Transferred models

---
+++ Vote/Models/OptionVoting.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Vote.Models
+{
+    public class OptionVoting
+    {
+        public string Id { get; set; }
+
+        [Required]
+        public string TitleVoting { get; set; }
+
+        [Required]
+        public string DescriptionVoting { get; set; }
+
+        public DateTime Start_dateVoting { get; set; } = DateTime.Now;
+
+        public DateTime End_dateVoting { get; set; } = DateTime.Now;
+
+        public string[] listOptions = new string[5];
+
+
+    }
+}
